6a01c7abbbc0d3a70a7f9cde8800d1d7904eacce
KyriakosAlexandrou <kyriakos.a.projects@gmail.com>
2016-07-24 01:56:26 +0100

    Initial commit

---
+++ app/src/main/java/com/kyriakosalexandrou/ampersandtest/models/NewsFeed.java
@@ -0,0 +1,48 @@
+package com.kyriakosalexandrou.ampersandtest.models;
+
+import android.support.annotation.DrawableRes;
+
+/**
+ * Created by Kyriakos on 23/07/2016.
+ */
+public class NewsFeed {
+    @DrawableRes
+    private int mImageRes;
+    private String mTitle;
+    private boolean mIsFavourite;
+    private SocialAggregates mSocialAggregates;
+
+    public NewsFeed(int imageRes, String title, boolean favourite, SocialAggregates socialAggregates) {
+        this.mImageRes = imageRes;
+        this.mTitle = title;
+        mIsFavourite = favourite;
+        mSocialAggregates = socialAggregates;
+
+        if (mIsFavourite) {
+            mSocialAggregates.incrementLike();
+        } else {
+            mSocialAggregates.DecreaseLike();
+        }
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    @DrawableRes
+    public int getImageRes() {
+        return mImageRes;
+    }
+
+    public SocialAggregates getAggregates() {
+        return mSocialAggregates;
+    }
+
+    public boolean isFavourite() {
+        return mIsFavourite;
+    }
+
+    public void setFavourite(boolean enabled) {
+        mIsFavourite = enabled;
+    }
+}
---
+++ app/src/main/java/com/kyriakosalexandrou/ampersandtest/widgetcomponents/AppTextView.java
@@ -0,0 +1,51 @@
+package com.kyriakosalexandrou.ampersandtest.widgetcomponents;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.kyriakosalexandrou.ampersandtest.R;
+
+/**
+ * Created by Kyriakos on 23/07/2016.
+ */
+public class AppTextView extends TextView {
+    private static final String TAG = AppTextView.class.getName();
+
+    public AppTextView(Context context) {
+        super(context);
+    }
+
+    public AppTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setCustomFont(context, attrs);
+    }
+
+    public AppTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setCustomFont(context, attrs);
+    }
+
+    private void setCustomFont(Context ctx, AttributeSet attrs) {
+        TypedArray a = ctx.obtainStyledAttributes(attrs, R.styleable.AppTextView);
+        String customFont = a.getString(R.styleable.AppTextView_customFont);
+        setCustomFont(ctx, customFont);
+        a.recycle();
+    }
+
+    public boolean setCustomFont(Context ctx, String asset) {
+        Typeface tf = null;
+        try {
+            tf = Typeface.createFromAsset(ctx.getAssets(), asset);
+        } catch (Exception e) {
+            Log.e(TAG, "Could not get typeface: " + e.getMessage());
+            return false;
+        }
+
+        setTypeface(tf);
+        return true;
+    }
+}
